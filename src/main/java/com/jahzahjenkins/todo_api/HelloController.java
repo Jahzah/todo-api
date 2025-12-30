115cd4462094c62a1c68469e11c60cc9e043f17a
Jahzah <jjenkins2343@gmail.com>
2025-12-29 21:29:47 -0600

    Initial commit: Spring Boot project setup with hello endpoint

---
+++ src/main/java/com/jahzahjenkins/todo_api/HelloController.java
@@ -0,0 +1,23 @@
+package com.jahzahjenkins.todo_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to Todo API!";
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, Spring Boot!";
+	}
+	
+	@GetMapping("/api/status")
+	public String status() {
+		return "API is running!";
+	}
+}
