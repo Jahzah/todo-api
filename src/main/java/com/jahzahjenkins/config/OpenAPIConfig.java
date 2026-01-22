8d1151cac2a8816ae12c0abb1c69e5f83fe80440
Jahzah <jjenkins2343@gmail.com>
2026-01-22 13:40:45 -0600

    Add API documentation with Swagger/SpringDoc

---
+++ src/main/java/com/jahzahjenkins/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.jahzahjenkins.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    
+    @Bean
+    public OpenAPI todoOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Todo API")
+                        .description("AI-Powered Todo Management System")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Jahzah Jenkins")
+                                .email("jjenkins2343@gmail.com")));
+    }
+}
